2e23bf6600a6e35d36c90a08b640b63cebb5f103
MisterZurg <misterzurg@gmail.com>
2024-01-30 23:06:35 +0300

    Solved intro/Combinations

--- problems/intro/combinations/src/lib.rs
+++ problems/intro/combinations/src/lib.rs
@@ -1,6 +1,22 @@
 #![forbid(unsafe_code)]
 
 pub fn combinations(arr: &[i32], k: usize) -> Vec<Vec<i32>> {
-    // TODO: your code goes here.
-    unimplemented!()
+    let mut result: Vec<Vec<i32>> = Vec::new();
+
+    get_combination(arr, 0, k, vec![], &mut result);
+
+    return result;
+}
+
+fn get_combination(arr: &[i32], l: usize, r: usize, comb: Vec<i32>, result: &mut Vec<Vec<i32>>){
+    if r == 0 {
+        result.push(comb);
+        return;
+    }
+
+    for i in l..arr.len() {
+        let mut tmp = comb.clone();
+        tmp.push(arr[i]);
+        get_combination(arr, i + 1, r - 1, tmp, result)
+    }
 }
